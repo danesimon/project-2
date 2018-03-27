89ed1e59f8fc29e266e23546ed3b1b8c94d486a2
shalsriv <shalsriv@SHALSRIV-US.in.oracle.com>
2018-03-26 20:21:24 -0400

    controller

--- controllers/musicController.js
+++ controllers/musicController.js
@@ -2,52 +2,42 @@ const express = require("express");
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-const cat = require("../models/cat.js");
+// Import the model (user.js) to use its database functions.
+const user = require("../models/user.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", (req, res) => {
-  cat.all(function(data) {
+  user.all(function(data) {
     const hbsObject = {
-      cats: data
+      users: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/cats", (req, res) => {
-  cat.create([
-    "name", "sleepy"
+router.post("/api/users", (req, res) => {
+  user.create([
+    "name"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name
   ], function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/cats/:id", (req, res) => {
+router.put("/api/users/:id", (req, res) => {
   const condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
-  cat.update({
-    sleepy: req.body.sleepy
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
 });
 
-router.delete("/api/cats/:id", (req, res) => {
+router.delete("/api/users/:id", (req, res) => {
   const condition = "id = " + req.params.id;
 
-  cat.delete(condition, function(result) {
+  user.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
